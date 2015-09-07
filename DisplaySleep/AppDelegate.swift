45c033ba1c58e5e148cf61563250cf138592df56
Hongze Xia <hongze.x@gmail.com>
2015-09-07 13:23:15 +1000

    Initial Commit

---
+++ DisplaySleep/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DisplaySleep
+//
+//  Created by Hongze Xia on 7/09/2015.
+//  Copyright (c) 2015 Hongze Xia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
